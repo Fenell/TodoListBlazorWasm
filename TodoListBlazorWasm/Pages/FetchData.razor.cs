8bfdd6dc22debd31735222a398b25872ef144614
Tuấn Đạt <datmtph29475@fpt.edu.vn>
2023-07-14 11:27:18 +0700

    Code Behind

---
+++ TodoListBlazorWasm/Pages/FetchData.razor.cs
@@ -0,0 +1,29 @@
+﻿using System.Net.Http.Json;
+using Microsoft.AspNetCore.Components;
+
+namespace TodoListBlazorWasm.Pages
+{
+	public partial class FetchData
+	{
+		private WeatherForecast[]? _forecasts;
+
+		//Có thể Inject trong class
+		[Inject] private HttpClient	Http { get; set; }
+
+		protected override async Task OnInitializedAsync()
+		{
+			_forecasts = await Http.GetFromJsonAsync<WeatherForecast[]>("sample-data/weather.json");
+		}
+
+		public class WeatherForecast
+		{
+			public DateTime Date { get; set; }
+
+			public int TemperatureC { get; set; }
+
+			public string? Summary { get; set; }
+
+			public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+		}
+	}
+}
